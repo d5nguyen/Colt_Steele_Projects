b8bd4371a5888a66fde2a4e2d9a021e53030c985
d5nguyen <d5nguyen@gmail.com>
2019-01-15 01:01:26 -0500

    more todo list stuff

--- list.js
+++ list.js
@@ -1,16 +1,40 @@
-var todos = [];
+var todos = ["Buy New Turtle"];
 var input = prompt("What would you like to do?");
 while(input !== "quit") {
     //handle input
     if(input === "list") {
-        console.log(todos);
+        listTodos();
     } else if ( input === "new"){
-        //ask for new todo
-        var newTodo = prompt("Enter new todo");
-        //add to todos array
-        todos.push(newTodo)
-    }
+        addTodo();
+    } else if (input === "delete"){
+        deleteTodo();
     //ask again for new input
     input = prompt("What would you like to do?");
     }
 console.log("Ok you quit the app");
+
+function listTodos(){
+    console.log("**********")
+    todos.forEach(function(todo, i){
+    console.log(i + ": " + todo);
+    });
+    console.log("**********");
+}
+
+function addTodo(){
+    //ask for new todo
+    var newTodo = prompt("Enter new todo");
+    //add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo");
+}
+    
+function deleteTodo(){
+    //ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    //delete that todo 
+    //splice()
+    todos.splice(index, 1);
+    console.log("Deleted Todo");
+    }
+}
